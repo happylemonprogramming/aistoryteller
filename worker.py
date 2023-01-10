bec348de0346db4c209cfea0672116f312bdb270
lemonknowsall <lemonknowsall@gmail.com>
2023-01-09 21:12:15 -0800

    updating to launch on heroku with random videos testing

--- worker.py
+++ worker.py
@@ -1,27 +1,24 @@
-# import os
-from rq import Worker, Queue, Connection
-import redis
+# Work needed
 from aistorytelling import aistorytelling
 
 # Background app running
 from rq import Queue, Worker, Connection
-# import subprocess
-# import random
 import redis
+import os
 
+# Redis server from Heroku Add-on
 r = redis.Redis(
   host='redis-15847.c258.us-east-1-4.ec2.cloud.redislabs.com',
   port=15847,
-  password='SQr6LnwBbwNWkdZSSfTksNw1BAzZBQgR')
+  password = os.environ["redisapikey"])
+# # Localhost Redis server using wsl
 # r = redis.Redis(host='localhost', port=6379, db=0)
+# redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+# conn = redis.from_url(redis_url)
 q = Queue(connection=r)
 listen = ['default']
 
-# redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-
-# conn = redis.from_url(redis_url)
-
-# Hopefully is passed variable of prompt
+# Variable 'prompt' passed from webapp (user input on index.html)
 import sys
 prompt = sys.argv[1]
 print(prompt)
