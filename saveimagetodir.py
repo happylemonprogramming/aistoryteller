027232d3f180d28ea30fd1250944c1f094c4d0c2
lemonknowsall <lemonknowsall@gmail.com>
2023-01-16 21:58:24 -0800

    updated image paths to display image on heroku

--- saveimagetodir.py
+++ saveimagetodir.py
@@ -5,7 +5,7 @@ def saveimagetodir(url,path,name):
     # url = 'https://hyperlink.com'
     img = Image.open(requests.get(url, stream = True).raw)
     # img.save(str(path)+str(name)+'.png')
-    img.save(f'{path}\{name}.png')
+    img.save(f'{path}/{name}.png')
     # file = str(path)+str(name)+'.png'
-    file = f'{path}\{name}.png'
+    file = f'{path}/{name}.png'
     return url, file
